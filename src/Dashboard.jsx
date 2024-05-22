9739b6815608a7f7c50ba38db9cdae27c117559a
server-craftsman <huyit2003@gmail.com>
2024-05-22 11:14:57 +0700

    delete all

--- src/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard() {
-    return (
-        <>
-            <h1>Thích Tâm Phúc đã đột nhập server!!!</h1>
-            <h1>WARNING!!!</h1>
-        </>
-    );
-}
-export default Dashboard;
