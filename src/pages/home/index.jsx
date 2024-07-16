bfbd47248b0906d10c35b8390027f2285c52e9f3
kietse172079 <kietntse172079@fpt.edu.vn>
2024-07-16 12:25:44 +0700

    Add carousel product in home-page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,20 @@
 import Carousel from "../../components/carousel";
 import Footer from "../../components/footer";
-// import Header from "../../components/header";
 import "./index.scss";
+import CarouselTimepiece from "../../components/carousel-product/Timepieces";
+import CarouselBridal from "../../components/carousel-product/Bridals";
+import CarouselDiamond from "../../components/carousel-product/Diamond";
+import CarouselRing from "../../components/carousel-product/Rings";
 
 function HomePage() {
   return (
     <div className="homepage">
-            <Carousel numberOfSlide={1} isUseNavigation={true} /> {/* Include the Carousel component */}
+      <Carousel numberOfSlide={1} isUseNavigation={true} />{" "}
+      <CarouselTimepiece numberOfSlide={5} isUseNavigation={true} />
+      <CarouselBridal numberOfSlide={5} isUseNavigation={true} />
+      <CarouselDiamond numberOfSlide={5} isUseNavigation={true} />
+      <CarouselRing numberOfSlide={5} isUseNavigation={true} />
       <Footer />
-
     </div>
   );
 }
